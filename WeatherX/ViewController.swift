6220422248b9338789d16186256d7e762cbb173a
Vu Duong <vuduong@Vus-MacBook-Pro.local>
2018-09-11 23:00:25 -0400

    First Commit

---
+++ WeatherX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherX
+//
+//  Created by Vu Duong on 9/11/18.
+//  Copyright © 2018 Vu Duong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
